f494beedf8fb0c8ca9c6ded2eb9cc82910745609
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-10-18 16:55:31 -0300

    created dynamic links
    created dynamic links

--- filme/urls.py
+++ filme/urls.py
@@ -1,8 +1,11 @@
 # url - view - template
 from django.urls import path, include
-from .views import Homepage, Homefilmes
+from .views import Homepage, Homefilmes, Detalhesfilmes
+
+app_name = 'filme'
 
 urlpatterns = [
-    path('', Homepage.as_view()),
-    path('filmes/', Homefilmes.as_view()),
+    path('', Homepage.as_view(), name='homepage'),
+    path('filmes/', Homefilmes.as_view(), name='homefilme'),
+    path('filmes/<int:pk>', Detalhesfilmes.as_view(), name='detalhesfilme'),
 ]
--- filme/views.py
+++ filme/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from .models import Filme
-from django.views.generic import TemplateView,ListView
+from django.views.generic import TemplateView, ListView, DetailView
 
 # Create your views here.
 
 class Homepage(TemplateView):
     template_name = "homepage.html"
 
-
 class Homefilmes(ListView):
     template_name = "homefilmes.html"
     model = Filme
-    # object_list
+    # object_list -> Lista de itens de modelo
+
+# View para lista de detalhes
+class Detalhesfilmes(DetailView):
+    template_name = "detalhesfilmes.html"
+    model = Filme
+    # object -> 1 item do nosso modelo
